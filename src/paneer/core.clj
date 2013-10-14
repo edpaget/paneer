dc68993c1d732ed7a1f9068075f15017f9684d5b
Edward Paget <ed.paget@gmail.com>
2013-10-14 16:20:09 -0500

    Add basic dsl for working with tables

--- src/paneer/core.clj
+++ src/paneer/core.clj
@@ -1,6 +1,121 @@
-(ns paneer.core)
+(ns paneer.core
+  (:refer-clojure :exclude [bigint boolean char double float time]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn- command
+  [command {:keys [if-exists]}]
+  {:command command
+   :if-exists (or if-exists false)
+   :table nil
+   :columns []})
+
+(defn create*
+  "Starts a CREATE TABLE command"
+  [& opts]
+  (command :create opts)) 
+
+(defn alter*
+  "Starts an ALTER TABLE command"
+  [& opts]
+  (command :alter opts))
+
+(defn drop*
+  "Starts a DROP TABLE command"
+  [& opts]
+  (command :drop opts))
+
+(defn table*
+  "Modifyes a command map to include table name"
+  [command table-name]
+  (merge command {:table (name table-name)}))
+
+(defn column
+  "Adds a column to a command map"
+  [command col-name & [col-type & options]]
+  (let [column-def {:col-name (name col-name)} 
+        column-def (merge column-def (when col-type 
+                                       {:type (name col-type) 
+                                        :options (into [] options)}))]
+    (update-in command [:columns] conj column-def)))
+
+(defn- must-be-alter
+  [command]
+  (when-not (= (:command command) :alter)
+    (throw (Exception. "Command Must be :alter"))))
+
+(defn drop-column*
+  "Specifies column to drop in ALTER TABLE command"
+  [command col-name]
+  (must-be-alter command)
+  (column (update-in command [:command] (constantly :alter-drop-column)) col-name))
+
+(defn rename-to*
+  "Renames table as part of ALTER TABLE command"
+  [command new-table]
+  (must-be-alter command)
+  (when-not (:table command)
+    (throw (Exception. "Must Define Table to Rename")))
+  (merge command {:new-table (name new-table) :command :alter-rename}))
+
+(defn rename-column-to*
+  "Renames a column as part of ALTER TABLE command"
+  [command col-name]
+  (must-be-alter command)
+  (when (empty? (:columns command))
+    (throw (Exception. "Must provide a target column to rename")))
+  (column (update-in command [:command] (constantly :alter-rename-column)) col-name))
+
+(defn add-column*
+  "Adds a column as part of a ALTER TABLE Command"
+  [command & col-options]
+  (must-be-alter command)
+  (let [command (update-in command [:command] (constantly :alter-create-column))] 
+    (apply column command col-options)))
+
+;; Helper Functions for Creating specifically typed columns
+
+(defn integer
+  "Creates an Integer Column"
+  [command col-name & options]
+  (column command col-name :integer options))
+
+(defn varchar
+  "Creates a Varchar Column"
+  [command col-name length & options]
+  (column command col-name (str "varchar(" length ")") options))
+
+(defn float
+  "Creates a Float Column"
+  [command col-name & options]
+  (column command col-name :float options))
+
+(defn timestamp
+  "Creates a timestamp column"
+  [command col-name & options]
+  (column command col-name :timestamp options))
+
+(defn serial
+  "Creates a serial column"
+  [command col-name & options]
+  (column command col-name :serial options))
+
+(defn text
+  "Creates a text column"
+  [command col-name & options]
+  (column command col-name :serial options))
+
+(defn bigint
+  "Creates a bigint column"
+  [command col-name & options]
+  (column command col-name :bigint options))
+
+(defn boolean
+  "Creates a boolean column"
+  [command col-name & options]
+  (column command col-name :boolean options))
+
+(defn double
+  "Creates a double precision column"
+  [command col-name & options]
+  (column command col-name "double precision" options))
+
+
--- test/paneer/core_test.clj
+++ test/paneer/core_test.clj
@@ -1,7 +1,64 @@
 (ns paneer.core-test
+  (:refer-clojure :exclude [bigint boolean char double float time]) 
   (:require [clojure.test :refer :all]
             [paneer.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest create*-test
+  (is (= (create*) {:command :create :if-exists false :table nil :columns []}))
+  (is (= (create* :if-exists true) 
+         {:command :create :if-exists true :table nil :columns []})))
+
+(deftest table*-test
+  (is (= (table* (create*) :new-table)
+         {:command :create :if-exists false :table "new-table" :columns []})))
+
+(deftest column-test
+  (is (= (column (create*) :id))
+      {:command :create
+       :if-exists false
+       :table nil
+       :columns [{:col-name "id"}]})
+  (is (= (column (create*) :id :serial :not-null :primary-key)
+         {:command :create 
+          :if-exists false 
+          :table nil 
+          :columns [{:type "serial" :col-name "id" :options [:not-null :primary-key]}]})))
+
+(deftest alter*-test
+  (is (= (alter*) {:command :alter :if-exists false :table nil :columns []}))
+  (is (= (alter* :if-exists true) 
+      {:command :alter :if-exists true :table nil :columns []})))
+
+(deftest drop*-test
+  (is (= (drop*) {:command :drop :if-exists false :table nil :columns []}))
+  (is (= (drop* :if-exists true) 
+      {:command :drop :if-exists true :table nil :columns []}))) 
+
+(deftest drop-column*-test
+  (is (= (drop-column* (alter*) "name")
+         {:command :alter-drop-column 
+          :table nil 
+          :if-exists false 
+          :columns [{:col-name "name"}]})))
+
+(deftest rename-to*-test
+  (is (= (rename-to* (table* (alter*) "users") :bad_users)
+         {:command :alter-rename
+          :table "users" 
+          :new-table "bad_users" 
+          :if-exists false 
+          :columns []})))
+
+(deftest rename-column-to*-test
+  (is (= (rename-column-to* (column (alter*) :name) :bad_name)
+         {:command :alter-rename-column
+          :table nil
+          :if-exists false
+          :columns [{:col-name "name"} {:col-name "bad_name"}]})))
+
+(deftest add-column*-test
+  (is (= (add-column* (alter*) "name")
+         {:command :alter-create-column
+          :table nil
+          :if-exists false
+          :columns [{:col-name "name"}]})))
