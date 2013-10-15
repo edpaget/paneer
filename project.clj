dedf7bd03d84b0eef58b8c2405c2657cca8e0f5c
Edward Paget <ed.paget@gmail.com>
2013-10-15 11:08:56 -0500

    Add codox documentation

--- project.clj
+++ project.clj
@@ -1,8 +1,12 @@
 (defproject paneer "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "GNU General Public License"
+            :url "https://www.gnu.org/licenses/gpl-3.0.html"}
+  :codox {:exclude [paneer.engine]
+          :src-dir-uri "https://github.com/edpaget/paneer/blob/master"
+          :src-linenum-anchor-prefix "L"}
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
                                   [org.clojure/java.jdbc "0.3.0-alpha5"]
-                                  [com.h2database/h2 "1.3.164"]]}})
+                                  [com.h2database/h2 "1.3.164"]]
+                   :plugins [[codox "0.6.6"]]}})
